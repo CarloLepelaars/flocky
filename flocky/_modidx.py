0f6f3294690e5c869326836964b525c957c12044
Carlo Lepelaars <info@carlolepelaars.nl>
2025-03-28 19:16:55 +0100

    get submissions. Simplify structure

--- flocky/_modidx.py
+++ flocky/_modidx.py
@@ -5,4 +5,12 @@ d = { 'settings': { 'branch': 'main',
                 'doc_host': 'https://CarloLepelaars.github.io',
                 'git_url': 'https://github.com/CarloLepelaars/flocky',
                 'lib_path': 'flocky'},
-  'syms': {'flocky.api': {'flocky.api.get_task': ('api.html#get_task', 'flocky/api.py')}, 'flocky.core': {}, 'flocky.gpu': {}}}
+  'syms': { 'flocky.api': { 'flocky.api.get_sub_df': ('api.html#get_sub_df', 'flocky/api.py'),
+                            'flocky.api.get_subs': ('api.html#get_subs', 'flocky/api.py'),
+                            'flocky.api.get_task': ('api.html#get_task', 'flocky/api.py'),
+                            'flocky.api.list_finalized_tasks': ('api.html#list_finalized_tasks', 'flocky/api.py'),
+                            'flocky.api.list_open_tasks': ('api.html#list_open_tasks', 'flocky/api.py'),
+                            'flocky.api.parse_subs': ('api.html#parse_subs', 'flocky/api.py'),
+                            'flocky.api.pd_parse': ('api.html#pd_parse', 'flocky/api.py')},
+            'flocky.core': {'flocky.core.get_gpu_info': ('core.html#get_gpu_info', 'flocky/core.py')},
+            'flocky.gpu': {}}}
